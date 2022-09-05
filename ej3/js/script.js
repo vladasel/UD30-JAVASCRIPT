d8be34280788cbb08c6dda2bfc33a49b502b95c0
vladasel <ionelavladasel2710@gmail.com>
2022-09-05 20:59:21 +0200

    done ej3

---
+++ ej3/js/script.js
@@ -0,0 +1,8 @@
+meses = ["enero", "febrero", "marzo","abril","mayo","junio","julio",
+"agosto","setiembre","octubre","noviembre","diciembre"];
+
+
+
+meses.forEach(mes => {
+    console.log(mes) 
+ });
