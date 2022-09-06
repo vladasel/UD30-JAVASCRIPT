de657db7bd1aff85401e4e5314912d9392d441c6
vladasel <ionelavladasel2710@gmail.com>
2022-09-06 12:59:20 +0200

    done ej 8

---
+++ ej8/js/script.js
@@ -0,0 +1,8 @@
+function esPar(n){
+    if(n%2==0){
+        document.getElementById('resultado').innerHTML =   "el numero es par";
+    
+    }else{
+        document.getElementById('resultado').innerHTML =   "el numero es impar";
+    }
+}
